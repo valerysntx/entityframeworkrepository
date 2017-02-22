60c1a23329d1591616b4468cb166bdc498eb9cce
Valery Schepaschenko <valery.sntx@gmail.com>
2017-02-22 20:06:55 +0200

    added tests project with initial fakedb test

---
+++ entityframeworkrepository.tests/FakeDbSetTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using entityframeworkrepository.core;
+using NUnit.Framework;
+
+namespace entityframeworkrepository.tests
+{
+    [TestFixture]
+    public partial class FakeDbSetTests
+    {
+
+        [Test]
+        public void ShouldCreateFakeDbSet_Of_TypeParamT ()
+        {
+            Assert.IsNotNull(new FakeDbSet<BaseEntity>());
+        }
+
+    }
+}
