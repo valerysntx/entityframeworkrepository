73b9c931e35acd5d202f2e02c6fc50332e02676c
Valery Schepaschenko <valery.sntx@gmail.com>
2017-02-23 05:53:23 +0200

    crud service tests using fake dbset

---
+++ entityframeworkrepository.tests/EntityServicesTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using entityframeworkrepository.core.entity;
+using entityframeworkrepository.core.service;
+using Moq;
+using NUnit.Framework;
+
+namespace entityframeworkrepository.tests
+{
+    [TestFixture]
+    public class EntityServicesTests
+    {
+        private Mock<IEntityService<BaseEntity>> _serviceMock;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _serviceMock = new Mock<IEntityService<BaseEntity>>();
+        }
+
+
+
+        [Test]
+        public void Person_Get_All_Using_FakeDbSet()
+        {
+            //Arrange
+            _serviceMock?.Setup(x => x.GetAll())?.Returns(
+                 new List<BaseEntity>(new [] { new FakeDbSet<Person>().Create() })
+               );
+
+            //Act
+            var result = _serviceMock?.Object?.GetAll();
+
+            //Assert
+            Assert.AreEqual(result.ToArray().Length, 1);
+        }
+
+    }
+}
