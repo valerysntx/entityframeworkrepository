63fa3912af03d7a7053dc833e434d15ab1efc4c2
Valery Schepaschenko <valery.sntx@gmail.com>
2017-02-25 13:25:29 +0200

    entityframeworkrepository.cache implementation

---
+++ entityframeworkrepository.cache/CacheKey.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace entityframeworkrepository.cache
+{
+    /// <summary>
+    /// A class representing a unique key for cache items.
+    /// </summary>
+    public class CacheKey : ICacheKey
+    {
+        private readonly string _key;
+        private readonly HashSet<ICacheTag> _tags;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheKey"/> class.
+        /// </summary>
+        /// <param name="key">The key for a cache item.</param>
+        public CacheKey(string key): this(key, Enumerable.Empty<string>()){
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheKey"/> class.
+        /// </summary>
+        /// <param name="key">The key for a cache item.</param>
+        /// <param name="tags">The tags for the cache item.</param>
+        public CacheKey(string key, IEnumerable<string> tags)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+            if (tags == null)
+                throw new ArgumentNullException("tags");
+
+            _key = key;
+
+            var cacheTags = tags.Select(k => new CacheTag(k));
+            _tags = new HashSet<ICacheTag>(cacheTags);
+        }
+
+
+        /// <summary>
+        /// Gets the key for a cached item.
+        /// </summary>
+        public string Key
+        {
+            get { return _key; }
+        }
+
+        /// <summary>
+        /// Gets the tags for a cached item.
+        /// </summary>
+        public HashSet<ICacheTag> Tags
+        {
+            get { return _tags; }
+        }
+
+        HashSet<ICacheTag> ICacheKey.Tags
+        {
+            get { return new HashSet<ICacheTag>(_tags); }
+        }
+    }
+}
---
+++ entityframeworkrepository.cache/CacheTag.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace entityframeworkrepository.cache
+{
+    /// <summary>
+    ///  CacheTag
+    /// </summary>
+    public class CacheTag : ICacheTag
+    {
+        private readonly string _tag;
+
+        public virtual string Tag
+        {
+            get { return _tag; }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheTag"/> class.
+        /// </summary>
+        /// <param name="tag">The tag name.</param>
+        public CacheTag(string tag)
+        {
+            if (tag == null)
+                throw new ArgumentNullException("tag");
+
+            _tag = tag;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        bool Equals(ICacheTag other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return string.Equals(other.Tag, Tag, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != typeof(CacheTag))
+                return false;
+
+            return Equals((CacheTag)obj);
+        }
+
+        /// <summary>
+        /// Implements the operator ==.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator ==(CacheTag left, CacheTag right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Implements the operator !=.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(CacheTag left, CacheTag right)
+        {
+            return !Equals(left, right);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (Tag != null ? Tag.GetHashCode() : 0);
+        }
+
+        public bool Equals(CacheTag other)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return Tag;
+        }
+
+        bool IEquatable<ICacheTag>.Equals(ICacheTag other)
+        {
+            return Equals((CacheTag) other);
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="CacheTag"/> to <see cref="System.String"/>.
+        /// </summary>
+        /// <param name="cacheTag">The cache tag.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator string(CacheTag cacheTag)
+        {
+            return cacheTag.ToString();
+        }
+    }
+}
