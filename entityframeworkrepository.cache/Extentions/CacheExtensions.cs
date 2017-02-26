73cd6a3f7eb2222e8415d1fca74e8779dee3b936
Valery Schepaschenko <valery.sntx@gmail.com>
2017-02-26 20:45:47 +0200

    order by many

--- entityframeworkrepository.cache/Extentions/CacheExtensions.cs
+++ entityframeworkrepository.cache/Extentions/CacheExtensions.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Remoting.Channels;
 using entityframeworkrepository.cache.Query;
 
 namespace entityframeworkrepository.cache.Extentions
@@ -144,5 +149,108 @@ namespace entityframeworkrepository.cache.Extentions
             removed = manager.Remove(key) as IEnumerable<TEntity>;
             return query;
         }
+
+
+        /// <summary>
+        /// Multiple OrderBy on materialized data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="props"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> source, params Expression<Func<T, object>>[] props)
+        {
+            var enumerable = source as T[] ?? source.ToArray();
+            return props.ToArray().Aggregate(enumerable, (current, prop) => current.OrderBy(prop).ToArray());
+        }
+
+
+        /// <summary>
+        /// reverse
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="props"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> OrderByDescending<T>(this IEnumerable<T> source, params Expression<Func<T, object>>[] props)
+        {
+            var enumerable = source as T[] ?? source.ToArray();
+            return props.ToArray().Aggregate(enumerable, (current, prop) => current.OrderByDescending(prop).ToArray());
+        }
+
+        /// <summary>
+        /// OrderBy
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="props"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> OrderBy<T>(this IQueryable<T> source, params Expression<Func<T, object>>[] props)
+        {
+            var enumerable = source.AsQueryable();
+            IEnumerable<T> results = enumerable.AsEnumerable();
+            return props.ToArray().Aggregate(enumerable, (current, prop) => current.OrderBy(prop).AsQueryable());
+        }
+
+        public static IEnumerable<T> OrderByDescending<T>(this IQueryable<T> source, params Expression<Func<T, object>>[] props)
+        {
+            var enumerable = source.AsQueryable();
+            IEnumerable<T> results = enumerable.AsEnumerable();
+            return props.ToArray().Aggregate(enumerable, (current, prop) => current.OrderByDescending(prop).AsQueryable());
+        }
+
+
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string property)
+        {
+            return ApplyOrder<T>(source, property, "OrderBy");
+        }
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> source, string property)
+        {
+            return ApplyOrder<T>(source, property, "OrderByDescending");
+        }
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> source, string property)
+        {
+            return ApplyOrder<T>(source, property, "ThenBy");
+        }
+        public static IOrderedQueryable<T> ThenByDescending<T>(this IOrderedQueryable<T> source, string property)
+        {
+            return ApplyOrder<T>(source, property, "ThenByDescending");
+        }
+
+        /// <summary>
+        /// Lambda Expressions ordering
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="property"></param>
+        /// <param name="methodName"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IOrderedQueryable<T> ApplyOrder<T>(IQueryable<T> source, string property, string methodName)
+        {
+            string[] props = property.Split('.');
+            Type type = typeof(T);
+            ParameterExpression arg = Expression.Parameter(type, "x");
+            Expression expr = arg;
+            foreach (string prop in props)
+            {
+                // use reflection (not ComponentModel) to mirror LINQ
+                PropertyInfo pi = type.GetProperty(prop);
+                expr = Expression.Property(expr, pi);
+                type = pi.PropertyType;
+            }
+            Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+            LambdaExpression lambda = Expression.Lambda(delegateType, expr, arg);
+
+            object result = typeof(Queryable).GetMethods().Single(
+                    method => method.Name == methodName
+                            && method.IsGenericMethodDefinition
+                            && method.GetGenericArguments().Length == 2
+                            && method.GetParameters().Length == 2)
+                    .MakeGenericMethod(typeof(T), type)
+                    .Invoke(null, new object[] { source, lambda });
+            return (IOrderedQueryable<T>)result;
+        }
+
+
     }
 }
