e04035ca9c627103381669cf8012162dabc7e94c
Valery Schepaschenko <valery.sntx@gmail.com>
2018-03-17 03:50:49 +0200

    update respond.js to 1.4.2  - resolve GPL to MIT package licensing issue  - 'fossa' - automatic code analysis for license issues
    https://app.fossa.io/projects/git%2Bgithub.com%2Fvalerysntx%2Fentityframeworkrepository/refs/branch/master/0fdee061d64eaca50a65c58aa09fdac8f99c1ce0/issues/

--- entityframeworkrepository.webapp/Properties/AssemblyInfo.cs
+++ entityframeworkrepository.webapp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("entityframeworkrepository.webapp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ef caching strategies")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("entityframeworkrepository.webapp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright Valery Schepaschenko ©  valery.sntx@gmail.com 2017/2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
